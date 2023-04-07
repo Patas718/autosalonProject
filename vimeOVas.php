0f4e287fc40a6d78a154241f8f53e7c21b9802e0
Patas718 <74207961+Patas718@users.noreply.github.com>
2023-04-07 21:21:30 +0200

    Create vimeOVas.php

---
+++ vimeOVas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Víme o Vás</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta http-equiv="refresh" content="3; url=http://patas.xf.cz/autosalon/">
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap');
+
+h1{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-family: 'Quicksand', sans-serif;
+    user-select: none;
+}
+</style>
+<body>
+    <h1>Už o Vás víme a brzy se ozveme.</h1>
+</body>
+</html>
